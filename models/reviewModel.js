fae0750360efa8928b9133b87a6653a847d3c443
devmathiusso <dev.mathiusso@gmail.com>
2019-11-04 21:02:38 -0300

    Populating the reviews inside Tour model using Virtual Populate

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,7 +34,7 @@ const reviewSchema = new mongoose.Schema(
 );
 
 reviewSchema.pre(/^find/, function(next) {
-  this.populate({ path: "tour", select: "name" }).populate({
+  this.populate({
     path: "user",
     select: "name photo"
   });
